16e5ad69c4b23881b15a74d336a7455fa91105af
Zakaria Kasmi <zakkikasmi@gmail.com>
2023-07-04 07:31:09 +0100

    update images

--- Makefile
+++ Makefile
@@ -1,21 +1,23 @@
 # Makefile
 
-.PHONY: all
+HOME = "/home/sfinoe"
 
-all: .env
-	cd srcs && docker-compose up --build
 
-.env:
-	echo "DOMAIN_NAME=zkasmi.42.fr" > srcs/.env
-	echo "MYSQL_ROOT_PASSWORD=123456" >> srcs/.env
-	echo "MYSQL_DATABASE=wp" >> srcs/.env
-	echo "MYSQL_USER=tnzoto" >> srcs/.env
-	echo "MYSQL_PASSWORD=1234" >> srcs/.env
-	echo "WP_USER_NORMAL=miniBig" >> srcs/.env
-	echo "WP_PASSWORD_NORMAL=123456" >> srcs/.env
-	echo "DB_HOST=mariadb" >> srcs/.env
-	echo "WP_URL=localhost" >> srcs/.env
-	echo "WP_TITLE=My WordPress" >> srcs/.env
-	echo "WP_USER=bigBro" >> srcs/.env
-	echo "WP_PASSWORD=jyEcC@4dC@1" >> srcs/.env
-	echo "WP_EMAIL=mail@mail.com" >> srcs/.env
+all:
+	@mkdir -p $(HOME)/data/wordpress
+	@mkdir -p $(HOME)/data/mariadb
+	@docker-compose -f ./srcs/docker-compose.yml up --build
+
+down :
+	@docker-compose -f ./srcs/docker-compose.yml down
+
+clean :
+	@docker-compose -f ./srcs/docker-compose.yml down -v
+	@docker system prune -f
+
+fclean : clean
+	@docker rmi -f $(docker images -q)
+
+re : fclean all
+
+.PHONY: all clean fclean re down
--- srcs/requirements/wordpress/tools/configure.sh
+++ srcs/requirements/wordpress/tools/configure.sh
@@ -1,14 +1,12 @@
-# !/bin/bash
+#!/bin/bash
 
-sleep 40
+if [ ! -f /var/www/html/wp-config.php ]; then
+	cd /var/www/html;
+	wp core download --allow-root;
+	mv /tmp/wp-config.php /var/www/html/
+	wp core install --allow-root --url=zkasmi.42.fr --title=${WP_TITLE} --admin_user=${WP_ADMIN_LOGIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL}
+	wp user create --allow-root ${WP_USER_LOGIN} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASSWORD};
+	echo "Wordpress: set up!";
+fi
 
-wp core download --locale=en_US --path=/tmp/wordpress --allow-root
-wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$DB_HOST --path=/tmp/wordpress --allow-root --skip-check
-wp core install --url="$WP_URL" --title="$WP_TITLE" --admin_user="$WP_USER" --admin_password="$WP_PASSWORD" --admin_email="$WP_EMAIL" --path=/tmp/wordpress --allow-root
-
-# Add the following to create a new normal user:
-wp user create "$WP_USER_NORMAL" normal@mail.com --role=author --user_pass="$WP_PASSWORD_NORMAL" --path=/tmp/wordpress --allow-root
-
-
-
-mkdir -p /var/www/wordpress && chown -R www-data:www-data /var/www/wordpress && cp -r /tmp/wordpress/* /var/www/wordpress/ && php-fpm7.3 -F
+exec "$@"
