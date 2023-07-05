0a7fadc2cce3132f78853ece2e4d70d10e60ec12
Zakaria Kasmi <zakkikasmi@gmail.com>
2023-07-05 14:06:20 +0100

    final edits

--- srcs/requirements/wordpress/tools/configure.sh
+++ srcs/requirements/wordpress/tools/configure.sh
@@ -6,7 +6,7 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 	mv /tmp/wp-config.php /var/www/html/
 	wp core install --allow-root --url=zkasmi.42.fr --title=${WP_TITLE} --admin_user=${WP_ADMIN_LOGIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL}
 	wp user create --allow-root ${WP_USER_LOGIN} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASSWORD};
-	echo "Wordpress: set up!";
+	echo "Wordpress: done!";
 fi
 
 exec "$@"
